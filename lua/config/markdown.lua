943cf097e9bcea5925a971ed7162991512b3051b
Jim Hessin <jhessin@gmail.com>
2026-05-03 22:27:35 -0600

    Added markdown customizations and tab navigation between panes.

---
+++ lua/config/markdown.lua
@@ -0,0 +1,74 @@
+vim.o.textwidth = 80
+
+vim.keymap.set("n", "<up>", "<c-a>", { silent = true })
+vim.keymap.set("n", "<down>", "<c-x>", { silent = true })
+
+local function setupCharter()
+  -- vim.cmd("only")
+  vim.cmd("cd ~/repos/work-files")
+  vim.cmd("split Preterms/comments.md")
+  vim.cmd("vsplit Preterms/pretermNotes.md")
+  vim.cmd("split Preterms/DWDM_Channels.txt")
+  vim.cmd("3resize 15")
+  vim.cmd("2resize 10")
+  vim.cmd("1resize 5")
+end
+
+local function setup()
+  -- vim.cmd("only")
+  vim.cmd("cd ~/repos/work-files")
+end
+
+local function py()
+  vim.cmd("sp term://ipython")
+end
+
+local function setupPNI()
+  vim.cmd("only")
+  vim.cmd("cd ~/repos/work-files")
+  vim.cmd("split AutoCad/notes.md")
+  vim.cmd("1resize 10")
+end
+
+local function initJob()
+  vim.cmd.normal(vim.api.nvim_replace_termcodes("I## <ESC>^eea<CR><ESC>VS`jjo<ESC>j", true, true, true))
+end
+
+local function initJobs(count)
+  for _ = 1, count do
+    initJob()
+  end
+end
+
+local function initMap(count)
+  for _ = 1, count do
+    vim.cmd.normal(vim.api.nvim_replace_termcodes("I### <ESC>^eea<CR><ESC>VS`jjo<ESC>j", true, true, true))
+  end
+end
+
+vim.api.nvim_create_user_command("Py", function(_)
+  py()
+end, {})
+vim.api.nvim_create_user_command("CJ", "normal ?##<CR>A - submitted - closed<ESC>", {})
+-- vim.api.nvim_create_user_command('OJ', 'normal ?##<CR>$4bhd$<ESC>', {})
+vim.api.nvim_create_user_command("OJ", "normal ?##<CR>$4bhd$<ESC>", {})
+vim.api.nvim_create_user_command("HJ", "normal ?##<CR>A - HOLD - waiting<ESC>", {})
+vim.api.nvim_create_user_command("Rename", "normal IF_<ESC>pa_<ESC>A - *<ESC>", {})
+vim.api.nvim_create_user_command("InitJob", function(_)
+  initJob()
+end, {})
+vim.api.nvim_create_user_command("InitJobs", function(opts)
+  initJobs(opts.fargs[1])
+end, { nargs = 1 })
+vim.api.nvim_create_user_command("InitMap", function(opts)
+  initMap(opts.fargs[1])
+end, { nargs = 1 })
+vim.api.nvim_create_user_command("SetupCharter", function()
+  setupCharter()
+end, {})
+vim.api.nvim_create_user_command("Setup", function()
+  setup()
+end, {})
+vim.api.nvim_create_user_command("SetupPNI", function()
+  setupPNI()
+end, {})
