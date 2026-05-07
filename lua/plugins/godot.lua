68be4cc64534dec226581bb32bd6aa53788f8996
Jim Hessin <jhessin@gmail.com>
2026-05-07 17:00:11 -0600

    fixing godot?

--- lua/plugins/godot.lua
+++ lua/plugins/godot.lua
@@ -1,38 +1,36 @@
 return {
   {
     "neovim/nvim-lspconfig",
-        @class PluginLspOpts
     opts = {
-        @type lspconfig.options
-          servers = {
-          gdscript = {
-            force_setup = true,
-            single_file_support = false,
-            cmd = {'ncat', '127.0.0.1', '6008'},
-            root_dir = require('lspconfig.util').root_pattern('project.godot', '.git'),
-            filetypes = {'gd', 'gdscript', 'gdscript3'}
-          }
-        }
-      }
-  }
-  -- { "habamax/vim-godot" },
-  -- { "skywind3000/asyncrun.vim" },
-  -- { "teatek/gdscript-extended-lsp.nvim", opts = { view_type = "floating", picker = "snacks" } },
-  -- {
-  --   "folke/snacks.nvim",
-  --   opts = {
-  --     picker = {
-  --       sources = {
-  --         explorer = {
-  --           hidden = true, -- show hidden files
-  --           ignored = false, -- don't show gitignored files
-  --           exclude = { -- exclude specific patterns
-  --             "*.uid", -- glob pattern for files ending with .uid
-  --             "server.pipe", -- exact filename match
-  --           },
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
+      servers = {
+        gdscript = {
+          force_setup = true,
+          single_file_support = false,
+          cmd = { "ncat", "127.0.0.1", "6008" },
+          root_dir = require("lspconfig.util").root_pattern("project.godot", ".git"),
+          filetypes = { "gd", "gdscript", "gdscript3" },
+        },
+      },
+    },
+  },
+  { "habamax/vim-godot" },
+  { "skywind3000/asyncrun.vim" },
+  { "teatek/gdscript-extended-lsp.nvim", opts = { view_type = "floating", picker = "snacks" } },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true, -- show hidden files
+            ignored = false, -- don't show gitignored files
+            exclude = { -- exclude specific patterns
+              "*.uid", -- glob pattern for files ending with .uid
+              "server.pipe", -- exact filename match
+            },
+          },
+        },
+      },
+    },
+  },
 }
