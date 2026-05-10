c292966ef1f31fff0e5e2524df33b5f7eb96b66c
Jim Hessin <jhessin@gmail.com>
2026-05-09 20:43:45 -0600

    fixed godot hopefully

--- lua/plugins/godot.lua
+++ lua/plugins/godot.lua
@@ -6,7 +6,7 @@ return {
         gdscript = {
           force_setup = true,
           single_file_support = false,
-          cmd = { "ncat", "127.0.0.1", "6008" },
+          cmd = { "ncat", "localhost", "6005" },
           root_dir = require("lspconfig.util").root_pattern("project.godot", ".git"),
           filetypes = { "gd", "gdscript", "gdscript3" },
         },
