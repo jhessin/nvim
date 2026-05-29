75bf07237367aa1b209316573dfd27ac01cec295
Jim Hessin <jhessin@gmail.com>
2026-05-29 14:16:05 -0600

    Cleaned up options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,7 +10,3 @@ vim.keymap.set("n", "<leader>um", function()
 end, { desc = "Render Markdown" })
 
 vim.g.maplocalleader = ","
-
--- Force Neovim to render unaccessed semantic tags with standard code colors
-vim.api.nvim_set_hl(0, "@lsp.tag.unnecessary", {})
-vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", {})
