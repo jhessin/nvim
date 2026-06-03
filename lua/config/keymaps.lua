29f5cfd01ac6254f7c105472b7ac57b98407ff79
Jim Hessin <jhessin@gmail.com>
2026-06-03 05:40:26 -0600

    split terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,14 @@
 -- Add any additional keymaps here
 --
 vim.keymap.set("n", "<TAB>", "<C-W><C-W>")
+
+vim.keymap.set("n", "<leader>gb", function()
+  -- Open a bottom split
+  vim.cmd("botright split")
+  vim.cmd("resize 15")
+
+  -- Start terminal with Git Bash directly
+  vim.cmd([[terminal "C:/Users/]] .. os.getenv("USERNAME") .. [[/scoop/apps/git/current/bin/bash.exe" --login -i]])
+end, {
+  desc = "Open Git Bash",
+})
